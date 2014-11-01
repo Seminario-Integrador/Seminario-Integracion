32a08e7c751a2519f89022d72b0bb6e86282a5b3
Angie Melissa <amelisdl@gmail.com>
2014-11-01 16:37:56 -0500

    Archivo Instalación
    te entrego el archivo instalación funcionando :3
    Te quiero <3

---
+++ aplicacion/instalacion/instalacion.php
@@ -0,0 +1,63 @@
+<?php
+	$conexion = mysqli_connect("localhost","root","") or die(("Error " . mysqli_error($conexion)));
+	$crearBase="CREATE DATABASE alangame";
+	$result = mysqli_query($conexion, $crearBase);
+	if($result){
+		echo "La base de Datos ha sido creada exitosamente :3<br>";
+	}else{
+		echo "Algo ha fallado. Intentalo de nuevo :(<br>";
+	}
+
+	mysqli_close($conexion);
+	$conexion = mysqli_connect("localhost","root","","alangame") or die(("Error " . mysqli_error($conexion)));
+
+	if(mysqli_query($conexion, "CREATE TABLE docente(
+			nombre varchar(55) NOT NULL,
+			contrasenaSH1 varchar(40) NOT NULL,
+			codigoDocente varchar(8) NOT NULL,
+			PRIMARY KEY (codigoDocente)
+		)")){
+		echo "Se ha creado la tabla Docente exitosamente<br>";
+	}
+
+	if(mysqli_query($conexion, "CREATE TABLE usuario(
+			nombre varchar(55) NOT NULL,
+			correo varchar(60) NOT NULL,
+			contrasenaSH1 varchar(40) NOT NULL,
+			codigoUsuario varchar(8) NOT NULL,
+			descripcion varchar (140),
+			puntaje	int NOT NULL,
+			nivel int NOT NULL,
+			subnivel int NOT NULL,
+			PRIMARY KEY (codigoUsuario)
+		)")){
+		echo "Se ha creado la tabla Usuario exitosamente<br>";
+	}
+
+	if(mysqli_query($conexion, "CREATE TABLE medalla(
+			logro varchar(55) NOT NULL,
+			tipo varchar(55) NOT NULL,
+			codigoUsuario varchar(8) NOT NULL,
+			PRIMARY KEY (logro),
+			FOREIGN KEY(codigoUsuario) REFERENCES usuario(codigoUsuario)
+			ON UPDATE CASCADE
+			ON DELETE CASCADE
+		)")){
+		echo "Se ha creado la tabla Medalla exitosamente<br>";
+	}
+
+	if(mysqli_query($conexion, "CREATE TABLE curso(
+			nombreCurso varchar(30) NOT NULL,	
+			codigoDocente varchar(8),
+			codigoUsuario varchar(8),
+			PRIMARY KEY (nombreCurso, codigoUsuario),
+			FOREIGN KEY(codigoUsuario) REFERENCES usuario(codigoUsuario)
+			ON UPDATE CASCADE
+			ON DELETE CASCADE,
+			FOREIGN KEY(codigoDocente) REFERENCES docente(codigoDocente)
+			ON UPDATE CASCADE
+			ON DELETE CASCADE
+		)")){
+		echo "Se ha creado la tabla Curso exitosamente<br>";
+	}
+?>
