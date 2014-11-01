6a017bcf5bf03211b4aa181c19dfed1466cf6443
Angie Melissa <amelisdl@gmail.com>
2014-11-01 17:15:40 -0500

    Modelo: Rgistro
    :3

--- aplicacion/modelo/usuarioBD.php
+++ aplicacion/modelo/usuarioBD.php
@@ -1,3 +1,21 @@
 <?php
+	require "aplicacion/modelo/modelo.php";
+
+	class UsuarioBD extends Modelo{
+
+		public function registrar($nombre, $codigo, $email, $contraseña)
+		{
+			$this->conectar("localhost", "root", "", "alangame");
+			$this->consulta("INSERT INTO usuario VALUES('".$usuario."','".$email."','".$contraseña."','".$codigo."','','0','1','0')");
+			$this->desconectar();
+		}
+
+		public function login($value='')
+		{
+			# code...
+		}
+	}
+
+	//retornar array  posicion (nombre) o false
 	
 ?>
