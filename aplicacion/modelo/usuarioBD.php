625884bba9f7e0dc83be5adbb991bfbad0dbd1fe
Angie Melissa <amelisdl@gmail.com>
2014-11-01 17:26:04 -0500

    UsuarioBD nueva correcion

--- aplicacion/modelo/usuarioBD.php
+++ aplicacion/modelo/usuarioBD.php
@@ -6,14 +6,24 @@
 		public function registrar($nombre, $codigo, $email, $contrasena)
 		{
 			$this->conectar("localhost", "root", "", "alangame");
-			$this->consultar("INSERT INTO usuario VALUES('".$usuario."','".$email."','".$contrasena."','".$codigo."','','0','1','0')");
+			$this->consultar("INSERT INTO usuario VALUES('".$nombre."','".$email."','".$contrasena."','".$codigo."','','0','1','0')");
 			$this->desconectar();
 		}
 
 		public function login($nombre, $contrasena)
 		{
 			$this->conectar("localhost", "root", "", "alangame");
-			$aux=$this->consulta("SELECT ");
+			$aux=$this->consulta("SELECT nombre FROM usuario WHERE nombre");
+
+			/*
+			nombre varchar(55) NOT NULL,
+			correo varchar(60) NOT NULL,
+			contrasenaSH1 varchar(40) NOT NULL,
+			codigoUsuario varchar(8) NOT NULL,
+			descripcion varchar (140),
+			puntaje	int NOT NULL,
+			nivel int NOT NULL,
+			subnivel int NOT NULL,*/
 		}
 	}
 
