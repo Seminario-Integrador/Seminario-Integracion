2642ad26c79062ba0ac1ef4c7699f7ee6e6c03ee
Angie Melissa <amelisdl@gmail.com>
2014-11-01 17:39:14 -0500

    Corrección contraseña :3

--- aplicacion/modelo/usuarioBD.php
+++ aplicacion/modelo/usuarioBD.php
@@ -3,6 +3,12 @@
 
 	class UsuarioBD extends Modelo{
 
+		/**
+		*	Método que se encarga de crear la consulta para guardar un nuevo usuario en la Base de Datos.
+		*	@param $nombre - Nombre del usuario a registrar
+		*	@param $codigo - Codigo del usuario a registrar
+		*	@param $email - Email del usuario a registrar
+		*/
 		public function registrar($nombre, $codigo, $email, $contrasena)
 		{
 			$this->conectar("localhost", "root", "", "alangame");
@@ -10,10 +16,14 @@
 			$this->desconectar();
 		}
 
+		/**
+		*	Método que se encarga de crear la consulta para loguear un usuario en la Aplicación
+		*	@param $nombre - Nombre
+		*/
 		public function login($nombre, $contrasena)
 		{
 			$this->conectar("localhost", "root", "", "alangame");
-			$aux=$this->consultar("SELECT nombre FROM usuario WHERE nombre='".$nombre."' AND contrasenaSH1='".$contraseña."'");
+			$aux=$this->consultar("SELECT nombre FROM usuario WHERE nombre='".$nombre."' AND contrasenaSH1='".$contrasena."'");
 			$this->desconectar();
 			$cont=0;
 			$nombre="";
