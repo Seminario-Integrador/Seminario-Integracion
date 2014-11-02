fe3727189646b91e0c6eb0490de17ae0c7c25f8f
Gerson Lazaro <sherson63@gmail.com>
2014-11-02 16:12:56 -0500

    Registro funcionando
    Corrección de errores que no permitian funcionar el registro

--- aplicacion/modelo/usuarioBD.php
+++ aplicacion/modelo/usuarioBD.php
@@ -15,7 +15,11 @@
 		public function registrar($username, $nombre, $codigo, $email, $contrasena, $curso)
 		{
 			$this->conectar("localhost", "root", "", "alangame");
-			$this->consultar("INSERT INTO usuario VALUES('".$nombre."','".$username."','".$email."','".$contrasena."','".$codigo."',".$curso."','','0','1','0','perfil.jpg', '1')");
+			if($curso==""){
+				$this->consultar("INSERT INTO usuario VALUES('".$nombre."','".$username."','".$email."','".$contrasena."','".$codigo."',NULL,'','0','1','0','perfil.jpg', '1')");		
+			}else{
+				$this->consultar("INSERT INTO usuario VALUES('".$nombre."','".$username."','".$email."','".$contrasena."','".$codigo."','".$curso."','','0','1','0','perfil.jpg', '1')");	
+			}
 			$this->desconectar();
 		}
 
