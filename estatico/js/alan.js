6df6044448ef961fa6e0dcc63e22333a456af5d9
Gerson Lazaro <sherson63@gmail.com>
2014-11-01 20:58:04 -0500

    Corrección de Estilos

--- estatico/js/alan.js
+++ estatico/js/alan.js
@@ -27,10 +27,32 @@ function desplegar () {
 	}
 	activo = !activo;
 }
-
-left.addEventListener("click", desplegar);
-
-
-function cerrarSesion () {
-	document.cerrarS.submit();
+function mostrar () {
+	if(!activo){
+		if(window.innerWidth>=768){
+			left.style.width = "20%";
+			right.style.width = "80%";	
+		}else{
+			left.style.height = "100%";
+		}
+	}
 }
+function ocultar () {
+	if(!activo){
+		if(window.innerWidth>=1200){
+			right.style.width = "110rem";
+			left.style.width = "7rem";	
+		}else if(window.innerWidth>=992 && window.innerWidth<1200){
+			right.style.width = "90rem";
+			left.style.width = "7rem";	
+		}else if(window.innerWidth>=768 && window.innerWidth<992){
+			right.style.width = "69rem";
+			left.style.width = "6rem";	
+		}else{
+			left.style.height = "7rem";
+		}
+	}
+}
+left.addEventListener("mouseover", mostrar);
+left.addEventListener("mouseout", ocultar);
+left.addEventListener("click", desplegar);
