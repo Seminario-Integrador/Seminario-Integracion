8316d399ada71a302410d45195c6861166d26a81
Gerson Lazaro <sherson63@gmail.com>
2014-12-09 21:26:41 -0500

    Blockly version completa
    En el sync anterior blockly tenia una versión incompleta.

--- estatico/js/juego.js
+++ estatico/js/juego.js
@@ -357,6 +357,7 @@ function dibujarJuegoNivel1Subnivel1 () {
 	tablero.fillText("Enviar",200,460);
 	var toolbox = '<xml>';
 	toolbox += '  <block type="controls_if"></block>';
+	toolbox += '  <block type="avanzar"></block>';
 	toolbox += '  <block type="controls_whileUntil"></block>';
 	toolbox += '</xml>';
 	desplegarBlock();
